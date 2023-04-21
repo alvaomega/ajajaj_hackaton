5fbf16eb724193c70483fb4d350fdcbdb27ba404
Mikolaj_Marewicz <mikolaj.marewicz@gmail.com>
2023-04-21 18:17:03 +0200

    refacotr ?

--- hackaton/soulsit/urls.py
+++ hackaton/soulsit/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
 from . import views
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from soulsit import views as user_views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', user_views.home, name='home'),
+    path('register/', user_views.register, name='register'),
 ]
