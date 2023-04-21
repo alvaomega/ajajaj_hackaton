87f6e9fb4d5e7c3d03c3ca8390ec4a12b1085b4a
Michał Sikora <sikora.ii.michal@gmail.com>
2023-04-21 18:26:04 +0200

    api chat bot
    jeszcze nie działa

--- hackaton/soulsit/api/import openai.py
+++ hackaton/soulsit/api/import openai.py
@@ -1,12 +1,12 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 def suggest_specialists(project_description):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt="In your project, based on the description '" + project_description + "', what specialist roles should you have?",
-        max_tokens=1024,
+        max_tokens=100,
         n=1,
         stop=None,
         temperature=0.5,
